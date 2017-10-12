d08ed53cca4bda1b17f3ced15f5d7c0add0fd8fe
José Padilla <jpadilla@webapplicate.com>
2017-10-12 13:07:29 -0400

    Add json and index endpoints

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
 import csv
+import datetime
 
 import pymongo
-from flask import Flask, Response
+from flask import Flask, Response, jsonify, json
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+
+        return json.JSONEncoder.default(self, obj)
 
 
 app = Flask(__name__)
+app.json_encoder = JSONEncoder
 client = pymongo.MongoClient(os.getenv('MONGODB_URI'))
 db = client.get_default_database()
 
@@ -24,17 +34,44 @@ class Echo(object):
         return value
 
 
+def get_stats(path):
+    return db.stats.find({'path': path}).sort('created_at')
+
+@app.route('/')
+def index():
+    pipeline = [{'$group': {'_id': '$path'}}]
+    paths = db.stats.aggregate(pipeline)
+    data = {}
+
+    for path in paths:
+        data[path['_id']] = {
+            'json': '/stats/{}.json'.format(path['_id']),
+            'csv': '/stats/{}.csv'.format(path['_id'])
+        }
+
+    return jsonify({'data': data})
+
+
+@app.route('/stats/<path>.json')
+def stats_json(path):
+    data = []
+
+    for stat in get_stats(path):
+        data.append({
+            'date': stat['created_at'],
+            'value': stat['value']
+        })
+
+    return jsonify(data)
+
+
 @app.route('/stats/<path>.csv')
-def stats(path):
+def stats_csv(path):
     writer = csv.DictWriter(Echo(), fieldnames=['date', 'value'])
     writer.writeheader()
 
     def generate():
-        stats = db.stats.find({
-            'path': path
-        })
-
-        for stat in stats:
+        for stat in get_stats(path):
             yield writer.writerow({
                 'date': stat['created_at'],
                 'value': stat['value']
