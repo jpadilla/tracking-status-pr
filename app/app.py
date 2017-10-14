e99c98c5606bfe0e0f350eee2e7d39876f823733
José Padilla <jpadilla@webapplicate.com>
2017-10-14 09:37:52 -0400

    Add simple graphs

---
+++ app/app.py
@@ -0,0 +1,81 @@
+import os
+import csv
+
+import pymongo
+from flask import Flask, Response, jsonify, render_template
+
+from .utils import JSONEncoder, Echo, request_wants_json
+
+
+app = Flask(__name__)
+app.json_encoder = JSONEncoder
+client = pymongo.MongoClient(os.getenv('MONGODB_URI'))
+db = client.get_default_database()
+
+
+def get_stats(path):
+    return db.stats.find({'path': path}).sort('created_at')
+
+
+@app.route('/')
+def index():
+    paths = []
+    results = db.stats.aggregate([
+        {
+            '$group': {
+                '_id': {'path': '$path', 'label': '$label'}
+            }
+        }
+    ])
+
+    for result in results:
+        path = result['_id']['path']
+
+        paths.append({
+            '_id': path,
+            'slug': path.replace('.', '-'),
+            'label': result['_id']['label'],
+            'json_url': '/stats/{}.json'.format(path),
+            'csv_url': '/stats/{}.csv'.format(path)
+        })
+
+    if request_wants_json():
+        data = {}
+
+        for path in paths:
+            data[path['_id']] = {
+                'json_url': path['json'],
+                'csv_url': path['csv']
+            }
+
+        return jsonify({'data': data})
+
+    return render_template('index.html', paths=list(paths))
+
+
+@app.route('/stats/<path>.json')
+def stats_json(path):
+    data = []
+
+    for stat in get_stats(path):
+        data.append({
+            'date': stat['created_at'],
+            'value': stat['value']
+        })
+
+    return jsonify(data)
+
+
+@app.route('/stats/<path>.csv')
+def stats_csv(path):
+    writer = csv.DictWriter(Echo(), fieldnames=['date', 'value'])
+    writer.writeheader()
+
+    def generate():
+        for stat in get_stats(path):
+            yield writer.writerow({
+                'date': stat['created_at'],
+                'value': stat['value']
+            })
+
+    return Response(generate(), mimetype='text/csv')
---
+++ app/utils.py
@@ -0,0 +1,33 @@
+import datetime
+
+from flask import json, request
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
+
+        return json.JSONEncoder.default(self, obj)
+
+
+# https://docs.djangoproject.com/en/1.8/howto/outputting-csv/
+class Echo(object):
+    """
+    An object that implements just the write method of the file-like
+    interface.
+    """
+    def write(self, value):
+        """
+        Write the value by returning it, instead of storing
+        in a buffer.
+        """
+        return value
+
+
+def request_wants_json():
+    best = request.accept_mimetypes \
+        .best_match(['application/json', 'text/html'])
+    return best == 'application/json' and \
+        request.accept_mimetypes[best] > \
+        request.accept_mimetypes['text/html']
